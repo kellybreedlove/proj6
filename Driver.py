40ba1523a3d0dbe261c236ee96fa3526073f8850
kellybreedlove <kelly.breedlove@my.wheaton.edu>
2015-05-01 06:01:53 -0500

    added DatabaseUtils and test for it, updated Driver to pull out that code

--- Driver.py
+++ Driver.py
@@ -1,21 +1,5 @@
-aquimport sqlite3
 from Graph import *
-
-def musicV():
-    conn1 = sqlite3.connect('subset_artist_term.db')
-    conn2 = sqlite3.connect('subset_artist_similarity.db')
-    conn3 = sqlite3.connect('subset_track_metadata.db')
-    
-    with conn:    
-        
-        c = conn.cursor()    
-        c.execute('select * from artists')
-        
-        rows = c.fetchall()
-        for row in rows:
-            print row
+from DatabaseUtils import *
 
 if __name__ == '__main__':
-    g = Graph(musicV())
-    g.printGraph()
-
+    pass
---
+++ TestDatabaseUtils.py
@@ -0,0 +1,12 @@
+from DatabaseUtils import *
+import unittest
+
+"""Test DatabaseUtils.py"""
+class TestDatabaseUtils(unittest.TestCase):
+
+    """Test"""
+    def test_(self):
+        pass
+
+    if __name__ == '__main__':
+        unittest.main()
